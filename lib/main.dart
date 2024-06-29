09e66302a3a17999dae2dc6a8fc0e8e9b32d7ef5
cletus ola <cletushunsu@gmail.com>
2024-06-29 12:50:32 +0100

    updated file

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:todo/screens/todo_list.dart';
 
 
 void main(){
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -11,7 +12,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(),
+      home: const TodoList(),
     );
   }
 }
